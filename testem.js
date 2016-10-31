60303f942ea58db68012c1d8a7038b4ebfc50e68
Alex LaFroscia <alexlafroscia@nestlabs.com>
2016-10-31 16:33:30 -0700

    Enable Chrome for `ember test`

--- testem.js
+++ testem.js
@@ -1,13 +1,13 @@
 /* eslint-env node */
+
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  'framework': 'qunit',
+  'test_page': 'tests/index.html?nocontainer',
+  'disable_watching': true,
+  'launch_in_ci': [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
+  'launch_in_dev': [
+    'Chrome'
   ]
 };
