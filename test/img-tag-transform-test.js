6a7fa8189b989963f0af2960f1234f2927fd445e
Alex LaFroscia <alex@lafroscia.com>
2016-11-21 00:03:23 -0800

    Fix Node v4 support

--- test/img-tag-transform-test.js
+++ test/img-tag-transform-test.js
@@ -1,3 +1,5 @@
+/* eslint-env node, mocha */
+
 const expect = require('chai').expect;
 
 const compile = require('../lib/utils').compile;
@@ -6,8 +8,9 @@ const ImgTagTransform = require('../lib/htmlbars-plugins/img-tag-transform');
 describe('<img> tag transformation', function() {
   it('can transform an <img> into a {{pf-img}}', function() {
     return compile('img-tag-transform', [ImgTagTransform]).then((results) => {
-      const [ after, before ] = results;
-      
+      const after = results[0];
+      const before = results[1];
+
       expect(before).to.deep.equal(after);
     });
   });
