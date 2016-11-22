c757eff6c4ad2e55e0be3533eb948fc80c2672b2
Alex LaFroscia <alexlafroscia@nestlabs.com>
2016-11-21 21:42:40 -0800

    Refactor HTMLBars transform code again

--- test/img-tag-transform-test.js
+++ test/img-tag-transform-test.js
@@ -2,42 +2,48 @@
 
 const expect = require('chai').expect;
 
-const compile = require('../lib/utils').compile;
 const ImgTagTransform = require('../lib/htmlbars-plugins/img-tag-transform');
+const compile = require('../lib/utils').compile([ImgTagTransform]);
 
 describe('<img> tag transformation', function() {
   it('can transform an <img> into a {{pf-img}}', function() {
-    return compile('img-tag-transform', [ImgTagTransform]).then((results) => {
-      const after = results[0];
-      const before = results[1];
+    const target = compile(`
+      {{pf-img src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" }}
+    `);
+    const source = compile(`
+      <img src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" />
+    `);
 
-      expect(before).to.deep.equal(after);
-    });
+    expect(source).to.deep.equal(target);
   });
 
   it('can pass attributes down to the <img> tag', function() {
-    return compile('img-tag-transform-attrs', [ImgTagTransform]).then((results) => {
-      const after = results[0];
-      const before = results[1];
+    const target = compile(`
+      {{pf-img alt="some-alt-text" src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" }}
+    `);
+    const source = compile(`
+      <img alt="some-alt-text" src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" />
+    `);
 
-      expect(before).to.deep.equal(after);
-    });
+    expect(source).to.deep.equal(target);
   });
 
   it('can pass a class down to the <img> tag', function() {
-    return compile('img-tag-transform-class', [ImgTagTransform]).then((results) => {
-      const after = results[0];
-      const before = results[1];
+    const target = compile(`
+      {{pf-img class="some-class" src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" }}
+    `);
+    const source = compile(`
+      <img class="some-class" src="foo.png" srcset="foo.png 1x, foo@2x.png 2x" />
+    `);
 
-      expect(before).to.deep.equal(after);
-    });
+    expect(source).to.deep.equal(target);
   });
 
   it('does not transform an <img> without `srcset`', function() {
-    return compile('img-tag-transform-no-srcet', [ImgTagTransform]).then((results) => {
-      const file = results[0];
+    const source = compile(`
+      <img src="foo.png" />
+    `);
 
-      expect(file).to.be.empty
-    });
+    expect(source).to.be.empty;
   });
 });
